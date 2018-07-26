9cbf7cce41cc25dc1c467fd23c5aec3a12381316
Jinwoo Park (Curt) <www.jwpark.co.kr@gmail.com>
2018-07-26 16:30:50 +0900

    Features/test ci (#7)
    * Add warning/error lines for testing
    
    * Update test_pytest.py
    
    * Update test_pytest.py
    
    * Add warning/error lines for testing
    
    * Add pylint
    
    * Fix pylint warnings
    
    * Fix flake8 warnings
    
    * Fix pytest senario

--- src/test_pytest.py
+++ src/test_pytest.py
@@ -1,6 +1,19 @@
+"""
+Doc
+"""
+
+
 def plus_one(num):
+    """
+    Doc
+    """
     return num + 1
 
 
+# Make a warning at static analysis phase
 def test_plus_one():
-    assert plus_one(3) == 4
+    '''
+    DOc
+    '''
+    # Make an error at unnitest phase
+    assert plus_one(2) == 3
