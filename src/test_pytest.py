70a79dcf7b16dcc048eda3fdebd64f7e7eb98219
Jinwoo Park (Curt) <www.jwpark.co.kr@gmail.com>
2018-07-26 16:20:16 +0900

    Fix pylint warnings

--- src/test_pytest.py
+++ src/test_pytest.py
@@ -1,6 +1,16 @@
+"""
+Doc
+"""
+
 def plus_one(num):
+    """
+    Doc
+    """
     return num + 1
 # Make a warning at static analysis phase
 def test_plus_one():
+    '''
+    DOc
+    '''
     # Make an error at unnitest phase
     assert plus_one(3) == 3
